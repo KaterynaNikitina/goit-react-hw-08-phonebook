fe5e24ddb17212679b15bbf3a737be4211ceecfd
Katerina <Kateryna.e.nikitina@gmail.com>
2023-08-10 23:12:36 -0600

    routes added

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import './index.css';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { persistor, store } from 'redux/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
-// import Loader from 'components/Loader/Loader';
+import Loader from 'components/Loader/Loader';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Provider store={store}>   */}
-    {/* <PersistGate loading={<Loader />} persistor={persistor}> */}
+    <Provider store={store}>  
+    <PersistGate loading={<Loader />} persistor={persistor}>
       <BrowserRouter basename='/goit-react-hw-08-phonebook'>
     <App />
     </BrowserRouter>
-    {/* </PersistGate> */}
-    {/* </Provider> */}
+    </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
